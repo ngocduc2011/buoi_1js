313d7899f25799a2f89392892f8db617ba9ef084
ngocduc2011-buoi_2 <tranngocduc20112001@gmail.com>
2023-06-13 10:36:47 +0700

    init project

--- Statements/main.js
+++
@@ -1,8 +0,0 @@
-let a, b, c;
-a = 5;
-b = 6;
-c = a + b;
-document.getElementById("demo1").innerHTML = a+b;
-document.getElementById("demo2").innerHTML = c;
-//document.getElementById("demo2").innerHTML = a+c;//
-/*document.getElementById("demo2").innerHTML = b+c;*/
